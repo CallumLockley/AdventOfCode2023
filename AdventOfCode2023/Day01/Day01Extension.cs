c0b2deb3fe93cfef46ced108370e1c012d15b730
Callum Lockley <lockleycallum1@gmail.com>
2023-12-01 20:28:59 +0000

    Day01 Completed

---
+++ AdventOfCode2023/Day01/Day01Extension.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AdventOfCode2023.Day01
+{
+    public static class Day01Extension
+    {
+        private const string FilePath = "Day01\\data.txt";
+        public static int Solution()
+        {
+            string[] lines = File.ReadAllLines(FilePath);
+            int total = 0;
+
+            foreach (var item in lines)
+            {
+                var numbers = string.Concat(item.Where(Char.IsNumber));
+                var result = Convert.ToInt32(numbers[0].ToString() + numbers.Last().ToString());
+                total += result;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,5 @@
+﻿using AdventOfCode2023.Day01;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Day01Extension.Solution().ToString());
